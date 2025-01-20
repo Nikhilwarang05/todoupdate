ceea3d4c86e835fdda654d3b2fdceeb0f4beaa69
Nikhilwarang05 <nikhilajitwarang05@gmail.com>
2025-01-21 01:35:15 +0530

    add js part 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.plusbutton')
+const input = document.querySelector('input')
+const ol = document.querySelector('ol')
+
+
+console.log(button)
+console.log(ol)
+
+
+const handleAdd = (e) => {
+    const testToAdd = input.value
+
+    
+}
