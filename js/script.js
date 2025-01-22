8efdf2ff823aff2e202330275ec33032f83b1b26
Nikhil <85712798+Nikhilwarang05@users.noreply.github.com>
2025-01-22 11:01:12 +0530

    todobasic

--- js/script.js
+++ js/script.js
@@ -1,14 +1,46 @@
-const button = document.querySelector('.plusbutton')
-const input = document.querySelector('input')
-const ol = document.querySelector('ol')
+const button = document.querySelector(".plusbutton");
+const input = document.querySelector(".inputbar");
+const list = document.querySelector("ol");
+// dark mode
+const darkMode = document.querySelector(".switch")
+const body = document.querySelector("body")
 
-
-console.log(button)
-console.log(ol)
-
+input.addEventListener("keydown", (e) => {
+  const key = e.key;
+  if (key === "Enter") {
+    handleAdd();
+  }
+});
 
 const handleAdd = (e) => {
-    const testToAdd = input.value
+  const textToAdd = input.value;
 
-    
-}
+  if (textToAdd.length > 0) {
+    const ele = document.createElement("li");
+    const text_node = document.createTextNode(textToAdd);
+
+    ele.appendChild(text_node);
+    list.appendChild(ele);
+
+    const button = document.createElement("button");
+    button.innerHTML = "Done";
+    ele.appendChild(button);
+
+    button.addEventListener("click", (e) => {
+      ele.remove();
+    });
+
+    input.value = ''
+  } else {
+    window.alert ('Task can not be empty')
+  }
+
+};
+
+button.addEventListener("click", handleAdd);
+
+
+//dark mode
+darkMode.addEventListener('click',(e) => {
+    body.classList.toggle('dark')
+})
